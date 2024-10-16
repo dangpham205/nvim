c17d5839ccc8667393bb0940b75e1cfcfadde5cc
dangpham205 <haidawng205@gmail.com>
2024-10-17 06:58:40 +0700

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,9 @@
+vim.g.deprecation_warnings = true
+
+local keymap = vim.keymap
+keymap.set("i", "<C-BS>", "<C-w>")
+keymap.set("c", "<C-BS>", "<C-w>")
+keymap.set("i", "<C-H>", "<C-w>") -- using Ctrl+Backspace delete a word. ref:https://www.reddit.com/r/neovim/comments/prp8zw/using_ctrlbackspace_in_neovim/
+keymap.set("c", "<C-H>", "<C-w>")
 -- Options are automatically loaded before lazy.nvim startup Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
