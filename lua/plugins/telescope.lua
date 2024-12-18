eac168473377c97ef5f19e584a446180c4a2b2ab
Hai Dang <haidawng205@gmail.com>
2024-12-18 21:50:59 +0700

    update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,12 +6,27 @@ return {
         -- change a keymap
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
         -- add a keymap to browse plugin files
+        -- cwd: searched dir
         {
             "<leader>fp",
             function()
                 require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
             end,
             desc = "Find Plugin File",
+        },
+        {
+            "<leader>fs",
+            function()
+                require("telescope.builtin").live_grep({ grep_open_files = true })
+            end,
+            desc = "Grep string in current file",
+        },
+        {
+            "<leader>fS",
+            function()
+                require("telescope.builtin").grep_string({ cwd = require("lazy.core.config").options.root })
+            end,
+            desc = "Grep string",
         },
         {
             "<leader>fu",
